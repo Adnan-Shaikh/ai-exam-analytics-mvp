4d3f0458b926070aa64450241f4821567d0400df
Adnan shaikh <adnan12042006@gmail.com>
2026-02-18 15:28:31 +0530

    Update CORS with production Vercel URL

--- backend-analytics/app/main.py
+++ backend-analytics/app/main.py
@@ -27,7 +27,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://localhost:3000",
-        "https://exam-analytics.vercel.app",  # Add your Vercel domain
+        "https://ai-exam-analytics-mvp.vercel.app/",  # Add your Vercel domain
         "https://*.vercel.app"  # Allow all Vercel preview deployments
     ],
     allow_credentials=True,
